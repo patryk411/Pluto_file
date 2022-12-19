a49ac52c00cf32ee7601f7308aae1beb7f75b1ef
patryk411 <117459014+patryk411@users.noreply.github.com>
2022-12-19 12:49:17 +0100

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,15 @@
-const accordion = document.getElementsByClassName('container');
-
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+let buttonFaqContainer = document.querySelectorAll('.container-block-faq button');
+ 
+function toggleAccordion() {
+  const buttonFaq = this.getAttribute('aria-expanded');
+  
+  for (let i = 0; i < buttonFaqContainer.length; i++) {
+    buttonFaqContainer[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (buttonFaq == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
 }
+
+buttonFaqContainer.forEach(buttonFaq => buttonFaq.addEventListener('click', toggleAccordion));
